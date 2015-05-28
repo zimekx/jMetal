60f1e39fc051f5f83ab7b0f0ef5ad44987f610b9
ajnebro <ajnebro@outlook.com>
2015-05-28 16:45:06 +0200

    Adding operators to MOGA

--- jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/MOGA.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/MOGA.java
@@ -1,11 +1,17 @@
 package org.uma.jmetal.solution.experimental;
 
 import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.operator.Operator;
 
 /**
  * Created by ajnebro on 28/5/15.
  */
 public class MOGA<Solution> implements Algorithm<Solution> {
+  private Operator<Solution, Solution> mutation ;
+  private Operator<Solution, Solution> crossover ;
+  private Operator<Solution, Solution> selection ;
+
+
   @Override public void run() {
 
   }
