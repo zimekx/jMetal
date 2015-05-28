d4914697d7eca5dd3e13576efc47cba0de97be2c
ajnebro <ajnebro@outlook.com>
2015-05-28 17:05:39 +0200

    Working on ExpBinaryProblem

--- jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/problem/impl/ExpBinaryProblem.java
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/problem/impl/ExpBinaryProblem.java
@@ -1,9 +1,30 @@
 package org.uma.jmetal.solution.experimental.problem.impl;
 
+import org.uma.jmetal.solution.SolutionBuilder;
+import org.uma.jmetal.solution.SolutionEvaluator;
 import org.uma.jmetal.solution.experimental.problem.ExpProblem;
+
+import java.util.Collection;
 
 /**
  * Created by ajnebro on 28/5/15.
  */
-public class ExpBinaryProblem<Solution> implements ExpProblem<Solution> {
+public class ExpBinaryProblem<Solution>
+    implements ExpProblem<Solution>, SolutionBuilder<Solution>, SolutionEvaluator<Solution> {
+
+  @Override public Collection<Variable<Solution, ?>> getVariables() {
+    return null;
+  }
+
+  @Override public <Value> void prepare(Variable<Solution, Value> variable, Value value) {
+
+  }
+
+  @Override public Solution build() {
+    return null;
+  }
+
+  @Override public Collection<Objective<Solution, ?>> getObjectives() {
+    return null;
+  }
 }
