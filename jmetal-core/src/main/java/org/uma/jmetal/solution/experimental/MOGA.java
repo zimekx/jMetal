95a0700d04c22a8956d5bd96dc6f520c0d7e3d63
ajnebro <ajnebro@outlook.com>
2015-05-28 16:42:24 +0200

    Added class jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/MOGA.java

---
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/MOGA.java
@@ -0,0 +1,16 @@
+package org.uma.jmetal.solution.experimental;
+
+import org.uma.jmetal.algorithm.Algorithm;
+
+/**
+ * Created by ajnebro on 28/5/15.
+ */
+public class MOGA<Solution> implements Algorithm<Solution> {
+  @Override public void run() {
+
+  }
+
+  @Override public Solution getResult() {
+    return null;
+  }
+}
