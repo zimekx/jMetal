ef2c9c0d0828830c57a0e869fa3df49b010bc653
ajnebro <ajnebro@outlook.com>
2015-05-28 17:03:23 +0200

    Adding problems

---
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/problem/ExpProblem.java
@@ -0,0 +1,7 @@
+package org.uma.jmetal.solution.experimental.problem;
+
+/**
+ * Created by ajnebro on 28/5/15.
+ */
+public interface ExpProblem<Solution> {
+}
---
+++ jmetal-core/src/main/java/org/uma/jmetal/solution/experimental/problem/impl/ExpBinaryProblem.java
@@ -0,0 +1,9 @@
+package org.uma.jmetal.solution.experimental.problem.impl;
+
+import org.uma.jmetal.solution.experimental.problem.ExpProblem;
+
+/**
+ * Created by ajnebro on 28/5/15.
+ */
+public class ExpBinaryProblem<Solution> implements ExpProblem<Solution> {
+}
